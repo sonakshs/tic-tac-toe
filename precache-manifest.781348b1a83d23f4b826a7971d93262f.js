294221a6b167ddc0f141cc9c2a00c90ac22c1f16
sonaksh <sonaksh@swiftace.ai>
2019-07-23 12:57:57 +0530

    Updates

---
+++ precache-manifest.781348b1a83d23f4b826a7971d93262f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13352d5eb050a800bf75b826e405ac61",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "b40d387550f57a841585",
+    "url": "/tic-tac-toe/static/css/main.955d9c24.chunk.css"
+  },
+  {
+    "revision": "8725516db170dbb77d62",
+    "url": "/tic-tac-toe/static/js/2.6521d408.chunk.js"
+  },
+  {
+    "revision": "b40d387550f57a841585",
+    "url": "/tic-tac-toe/static/js/main.e1741435.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
